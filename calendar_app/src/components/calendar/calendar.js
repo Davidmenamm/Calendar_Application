e2d04f8a195358d32b77cf6c5451e2569a199746
Davidmenamm <davidmenam15@gmail.com>
2022-02-25 00:02:07 -0500

    FIX: simple changes.

--- calendar_app/src/components/calendar/calendar.js
+++ calendar_app/src/components/calendar/calendar.js
@@ -231,7 +231,7 @@ export const Calendar = () => {
                         multiline={true}
                         // minRows={1}
                         value={text1}
-                        onChange={e => setText1(e.target.value)}/>
+                        onChange={e => setText1(e.target.value - 1)}/>
                     {/* Time */}
                     <p>{'Time'}</p>
                     <TextField name='time'
